9ae4380364880a7fed010dd53b36f43fb21b4e76
SoubhagyaB <soubhagyabiswal.mca@gmail.com>
2020-05-19 20:02:44 +0530

    Committing all the test cases (Passed)

--- SpellTheNumber_Unit_Tests/SpellTheNumberUnitTest.cs
+++ SpellTheNumber_Unit_Tests/SpellTheNumberUnitTest.cs
@@ -18,7 +18,30 @@ namespace SpellTheNumber_Unit_Tests
             var expectedResult = "One Hundred And Twenty Three";
             Assert.AreEqual(expectedResult, actualResult);
         }
-        
+        [TestMethod]
+        public void Number_Thousand_Series_TestMethod()
+        {
+            var translated = new SpellTheNumber();
+            var actualResult = translated.ConvertToWords("13456");
+            var expectedResult = "Thirteen Thousand Four Hundred And Fifty Six";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void Number_Million_Series_TestMethod()
+        {
+            var translated = new SpellTheNumber();
+            var actualResult = translated.ConvertToWords("1233456");
+            var expectedResult = "One Million Two Hundred Thirty Three Thousand Four Hundred And Fifty Six";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod]
+        public void Number_Billion_Series_TestMethod()
+        {
+            var translated = new SpellTheNumber();
+            var actualResult = translated.ConvertToWords("1233445566");
+            var expectedResult = "One Billion Two Hundred Thirty Three Million Four Hundred Fourty Five Thousand Five Hundred And Sixty Six";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
         [TestMethod]
         [ExpectedException (typeof(NullReferenceException))]
         public void Number_Exception_TestMethod()
